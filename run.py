0991fa74c5d56df0cf2379b344be756f5005fc8e
Lanzhijiang <lanzhijiang@foxmail.com>
2020-06-26 11:06:22 +0800

    「添加」run.py启动文件

---
+++ run.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+# author: Lan_zhijiang
+# desc: The start file of ALR, you can choose start the client or the could right here.
+
+
+def client_launcher():
+
+    """
+    启动客户端
+    :return:
+    """
+
+
+client_launcher()
